46117c5ea1746a712453bbee57126c96e9db4b70
Andres` <andrespautrat2@gmail.com>
2019-11-18 15:02:35 -0500

    Probabilidades iniciales random

--- FightF.py
+++ FightF.py
@@ -2,6 +2,7 @@ import pygame, sys
 import time as t
 from pygame.locals import *
 import numpy as np
+import random as rnd
 
 scale = 50
 red = pygame.Color(255, 0, 0)
@@ -41,6 +42,14 @@ class Fighter:
     def __init__(self, postX, postY):
         self.postX = postX
         self.postY = postY
+        self.kick=[0.17,0.17,0.17,0.17,0.17,0.15]
+        self.punsh=[0.17,0.17,0.17,0.17,0.17,0.15]
+        self.avanza=[0.17,0.17,0.17,0.17,0.17,0.15]
+        self.retrocede=[0.17,0.17,0.17,0.17,0.17,0.15]
+        self.agacha=[0.17,0.17,0.17,0.17,0.17,0.15]
+        self.nada=[0.17,0.17,0.17,0.17,0.17,0.15]
+        self.pushEsquivados=0
+        self.punsh
 
     def crear(self, screen, parado, postX, postY):
         fighter_1 = (postX * scale, postY * scale, scale, 2 * scale + parado * scale)
@@ -57,6 +66,28 @@ class Fighter:
 
     def getY(self):
         return self.postY
+    
+    def reset(self,reseteando):
+        probabilidadRestante=100
+        print(0)
+        for i in range(len(reseteando)):
+            if(probabilidadRestante<=50):
+                reseteando[i]=rnd.randint(0,probabilidadRestante)/100
+            else:
+                reseteando[i]=rnd.randint(0,50)/100
+            probabilidadRestante-=reseteando[i]
+            
+        return
+    def resetAll(self):
+        self.reset(self.kick)
+        self.reset(self.punsh)
+        self.reset(self.avanza)
+        self.reset(self.retrocede)
+        self.reset(self.agacha)
+        self.reset(self.nada)
+        return
+        
+
 
 
 malla = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -94,7 +125,10 @@ def displayW(screen):
     postY2 = 8
     parado = True
     fighter_1 = Fighter(postX1, postY1)
+    fighter_1.resetAll()
     fighter_2 = Fighter(postX1, postY2)
+    print(len(fighter_1.agacha))
+    print(fighter_1.kick)
     while True:
         screen.fill(black)
         screen.blit(scenerescale, (0, 0))
